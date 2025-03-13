0b05aed3862abc87195c8f1480f047129e097a83
zhaemin <95492807+zhaemin@users.noreply.github.com>
2025-03-13 17:09:38 +0900

    Add files via upload

--- models/setfsl.py
+++ models/setfsl.py
@@ -5,18 +5,19 @@ from torchvision import models
 import torch.optim as optim
 import copy
 import math
+import random
 import numpy as np
 
 from utils import split_support_query_set
 import backbone.vit_encoder as vit_encoder
 
 class CrossAttention(nn.Module):
-    def __init__(self, embed_dim):
+    def __init__(self, input_dim, output_dim):
         super().__init__()
-        self.q = nn.Linear(embed_dim, embed_dim)
-        self.k = nn.Linear(embed_dim, embed_dim)
-        self.v = nn.Linear(embed_dim, embed_dim)
-        self.o = nn.Linear(embed_dim, embed_dim)
+        self.q = nn.Linear(input_dim, output_dim)
+        self.k = nn.Linear(input_dim, output_dim)
+        self.v = nn.Linear(input_dim, output_dim)
+        self.o = nn.Linear(output_dim, output_dim)
     
     def forward(self, x, y):
         q = self.q(x)
@@ -28,16 +29,19 @@ class CrossAttention(nn.Module):
         
         return z
 
-class SET_FSL(nn.Module):
+class SETFSL(nn.Module):
     def __init__(self, img_size, patch_size, num_objects, temperature, layer, with_cls=False, continual_layers=None, train_w_qkv=False, train_w_o=False):
-        super(SET_FSL, self).__init__()
+        super(SETFSL, self).__init__()
         self.img_size = img_size
         self.patch_size = patch_size
         self.num_patches = (self.img_size // self.patch_size) ** 2
         
         self.add_cls_token = True
         self.encoder = self.load_backbone()
-        self.outdim = self.encoder.embed_dim
+         
+        self.encoder_dim = self.encoder.embed_dim
+        self.ca_dim = self.encoder_dim
+        
         self.num_objects = num_objects
         self.temperature = temperature
         self.with_cls = with_cls
@@ -45,22 +49,34 @@ class SET_FSL(nn.Module):
         self.train_w_qkv = train_w_qkv
         self.train_w_o = train_w_o
         
-        self.object_queries = nn.Embedding(self.num_objects, self.encoder.embed_dim)
+        self.object_queries = nn.Embedding(self.num_objects, self.encoder_dim)
         self.layer = layer
         
-        self.norm1 = nn.LayerNorm(self.outdim)
-        self.norm2 = nn.LayerNorm(self.outdim)
-        self.crossattn = CrossAttention(self.outdim)
+        self.norm1 = nn.LayerNorm(self.encoder_dim)
+        self.norm2 = nn.LayerNorm(self.ca_dim)
+        self.crossattn = CrossAttention(self.encoder_dim, self.ca_dim)
         
         if continual_layers != None:
-            self.ca_blocks = nn.ModuleList([CrossAttention(self.outdim) for i in range(len(continual_layers))])
+            self.object_queries = nn.Embedding(self.num_objects, self.encoder_dim)
+            self.ca_blocks = nn.ModuleList([CrossAttention(self.encoder_dim, self.ca_dim) for i in range(len(continual_layers))])
             self.layer_nums = continual_layers
-            print(*self.layer_nums)
+            print('continual layers:', *self.layer_nums)
+            
+            for blk in self.ca_blocks:
+                if not self.train_w_qkv:
+                    self.make_qkv_identity(blk)
+                if not self.train_w_o:
+                    self.make_o_identity(blk)
         
+        if not self.train_w_qkv:
+            self.make_qkv_identity(self.crossattn)
+        if not self.train_w_o:
+            self.make_o_identity(self.crossattn)
         for name, param in self.encoder.named_parameters():
             param.requires_grad = False
         
-        print('num_object:', num_objects,' temerature:', temperature, ' layer:', layer, ' withcls:', with_cls, ' train_w_qkv:', train_w_qkv, ' train_w_o:', train_w_o)
+        print('num_object:', num_objects,' temerature:', temperature, ' layer:', layer, ' withcls:', with_cls, ' train_w_qkv:', train_w_qkv, ' train_w_o:', train_w_o,
+              'ca_dim:', self.ca_dim)
         
     def load_backbone(self):
         print('img_size: ',self.img_size)
@@ -78,39 +94,32 @@ class SET_FSL(nn.Module):
     def make_o_identity(self, crossattn):
         crossattn.o = nn.Identity()
     
+    def layernorm_affine_false(self):
+        self.encoder.norm
+    
     def individual_crossattn(self, z):
-        if not self.train_w_qkv:
-            self.make_qkv_identity(self.crossattn)
-        if not self.train_w_o:
-            self.make_o_identity(self.crossattn)
-            
         q = self.object_queries.weight
         kv = z[self.layer]
-        z = self.crossattn(q, kv)
+        x = self.crossattn(q, kv)
         
-        return z
+        return x
     
     def continual_crossattn(self, z):
-        object_queries = torch.zeros_like(self.object_queries.weight)
-        object_queries = object_queries + self.object_queries.weight
+        object_queries = self.object_queries.weight
+        x = object_queries
         
-        self.make_o_identity(self.crossattn)
         for blk, l in zip(self.ca_blocks, self.layer_nums):
-            if not self.train_w_qkv:
-                self.make_qkv_identity(blk)
-            if not self.train_w_o:
-                self.make_o_identity(blk)
-            q = object_queries
+            q = x
             kv = z[l]
-            x = self.crossattn(q, kv)
+            x = blk(q, kv)
             
-            object_queries = object_queries + x
+            x = x + q
             
         return x
         
     def forward(self, inputs, labels, device):
         with torch.no_grad():
-            z = self.norm1(self.encoder(inputs, return_attn=False, memories=True))
+            z = self.encoder(inputs, return_attn=False, memories=True)
         
         cls_token = z[self.layer][:, 0, :].unsqueeze(1)
         
@@ -133,7 +142,7 @@ class SET_FSL(nn.Module):
         for x_support, x_query, y_support, y_query in tasks:
             x_query = F.normalize(x_query, dim=-1)
             shots = x_support.size(0) // 5
-            prototypes = F.normalize(torch.mean(x_support.view(5, shots, -1, self.outdim), dim=1), dim=-1)
+            prototypes = F.normalize(torch.mean(x_support.view(5, shots, -1, self.ca_dim), dim=1), dim=-1)
             
             x_query = x_query.transpose(0, 1) # objects queries dim
             prototypes = prototypes.transpose(0, 1) # objects 5 dim
@@ -151,7 +160,7 @@ class SET_FSL(nn.Module):
             correct = 0
             total = 0
             
-            z = self.norm1(self.encoder(inputs, return_attn=False, memories=True))
+            z = self.encoder(inputs, return_attn=False, memories=True)
             
             cls_token = z[self.layer][:, 0, :].unsqueeze(1)
             
@@ -163,28 +172,20 @@ class SET_FSL(nn.Module):
             
             if self.with_cls:
                 z = torch.concat((z, cls_token), dim=1)
-                
+            
             tasks = split_support_query_set(z, labels, device, num_tasks=1, num_shots=5)
             
             loss = 0
             for x_support, x_query, y_support, y_query in tasks:
                 x_query = F.normalize(x_query, dim=-1)
                 shots = x_support.size(0) // 5
-                prototypes = F.normalize(torch.mean(x_support.view(5, shots, -1, self.outdim), dim=1), dim=-1)
+                prototypes = F.normalize(torch.mean(x_support.view(5, shots, -1, self.ca_dim), dim=1), dim=-1)
                 
                 x_query = x_query.transpose(0, 1) # objects queries dim
                 prototypes = prototypes.transpose(0, 1) # objects 5 dim
                 
                 distance = torch.einsum('oqd, owd -> oqw', x_query, prototypes).transpose(0, 1) # queries objects 5
                 
-                new_distance = []
-                for i in range(distance.size(0)):
-                    mean = distance[i].mean(dim=1, keepdim=True)
-                    var = distance[i].var(dim=1, keepdim=True)
-                    nd = (distance[i] - mean) / (var + 1.e-6)**.5
-                    new_distance.append(nd)
-            
-                distance = torch.stack(new_distance)
                 logits = distance.mean(dim=1)
                 
                 loss += F.cross_entropy(logits, y_query)
